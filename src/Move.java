f14be527dd45eaa16afcb0c053da581876729ea4
Youssef Moataz <yousefcrunshy03@gmail.com>
2023-02-16 23:26:33 +0200

    Added Move class.

---
+++ src/Move.java
@@ -0,0 +1,18 @@
+public class Move {
+    private Integer x;
+    private Integer y;
+
+    public Move(Integer X, Integer Y) {
+        this.x = X;
+        this.y = Y;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+}
--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Player {
 
     private String name;
@@ -9,8 +11,21 @@ public class Player {
         this.symbol = Symbol;
     }
 
-    public void getMove(int x, int y) {
+    public Move getMove(Integer BoardSize) {
 
+        Integer x = -1;
+        Integer y = -1;
+
+        Scanner moveScanner = new Scanner(System.in);
+
+        while (x < 0 && x > BoardSize - 1 && y < 0 && y > BoardSize - 1) {
+            System.out.println("Enter x-position");
+            x = moveScanner.nextInt();
+            System.out.println("Enter y-position");
+            y = moveScanner.nextInt();
+        }
+
+        return new Move(x, y);
     }
 
     public String getName() {
