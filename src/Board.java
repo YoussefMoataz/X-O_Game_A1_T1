c67450f951997acbd1c4d08f3599161a6f46dc43
Youssef Moataz <yousefcrunshy03@gmail.com>
2023-02-24 17:06:44 +0200

    Added Mohamed's code.

--- src/Board.java
+++ src/Board.java
@@ -1,12 +1,11 @@
-abstract class Board {
+abstract class Board
+{
     protected int size;
-    protected char grid[][];
+    protected char[][] grid;
 
     public abstract boolean update(int x, int y, char symbol);
-
     public abstract void displayBoard();
-
     public abstract void clearBoard();
-
-    public abstract boolean isWinner();
+    public abstract boolean isWinner(char symbol);
+    public abstract boolean isDraw();
 }
--- src/TicTacToeBoard.java
+++ src/TicTacToeBoard.java
@@ -56,30 +56,30 @@ public class TicTacToeBoard extends Board
             return false;
         }
     }
-    public boolean isWinner()
+    public boolean isWinner(char symbol)
     {
         boolean win = false;
         // horizontal check
         for(int i = 0; i < size; i++)
         {
-            win = (grid[i][0]) == (grid[i][1]) && grid[i][0] == grid[i][2] && grid[i][0] != ' ';
+            win = (grid[i][0]) == (grid[i][1]) && grid[i][0] == grid[i][2] && grid[i][0] == symbol;
             if(win){break;}
         }
         if(win){return win;}
         // vertical check
         for (int i = 0; i < size; i++)
         {
-            win = (grid[0][i] == grid[1][i]) && ((grid[0][i] == grid[2][i]) && (grid[0][i] != ' '));
+            win = (grid[0][i] == grid[1][i]) && ((grid[0][i] == grid[2][i]) && (grid[0][i] == symbol));
             if(win){break;}
         }
         if(win){return win;}
 
         // diagonal check
 
-        win = (grid[0][0]) == (grid[1][1]) && ((grid[0][0] == grid[2][2]) && (grid[0][0] != ' '));
+        win = (grid[0][0]) == (grid[1][1]) && ((grid[0][0] == grid[2][2]) && (grid[0][0] == symbol));
         if(win){return win;}
 
-        win = (grid[0][2]) == (grid[1][1]) && ((grid[0][2] == grid[2][0]) && (grid[0][2] != ' '));
+        win = (grid[0][2]) == (grid[1][1]) && ((grid[0][2] == grid[2][0]) && (grid[0][2] == symbol));
 
         return win;
     }
