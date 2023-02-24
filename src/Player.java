ae0125e7b4f9ea263fdaf0550218a6050be7f1bc
Youssef Moataz <yousefcrunshy03@gmail.com>
2023-02-24 16:18:37 +0200

    Added Hassan's code.

--- src/Player.java
+++ src/Player.java
@@ -1,37 +1,38 @@
 import java.util.Scanner;
 
 public class Player {
-
-    private final String name;
-    private final char symbol;
-
-    public Player(String Name, char Symbol) {
-        this.name = Name;
-        this.symbol = Symbol;
+    private String Name;
+    private char Symbol;
+    //constructor
+    public Player(String name,char symbol){
+        Name = name;
+        Symbol = symbol;
+    }
+    private int getX(){
+        System.out.printf("enter x");
+        Scanner sc =new Scanner(System.in);
+        int x =sc.nextInt();
+        return x;
+    }
+    private int gety(){
+        System.out.printf("enter y");
+        Scanner sc =new Scanner(System.in);
+        int y =sc.nextInt();
+        return y;
+    }
+    public String getName(){
+        return Name;
+    }
+    public char getSymbol(){
+        return Symbol;
     }
 
-    public Move getMove() {
+    public Move getMove(){
 
-        Integer x = -1;
-        Integer y = -1;
-
-        Scanner moveScanner = new Scanner(System.in);
-
-        System.out.println("Enter x-position");
-        x = moveScanner.nextInt();
-
-        System.out.println("Enter y-position");
-        y = moveScanner.nextInt();
+        int x = getX();
+        int y = gety();
 
         return new Move(x, y);
-    }
 
-    public String getName() {
-        return name;
     }
-
-    public char getSymbol() {
-        return symbol;
-    }
-
 }
