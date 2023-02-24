1b993945a02d6ecc21ed22b3275cf48343564317
Youssef Moataz <yousefcrunshy03@gmail.com>
2023-02-24 15:29:44 +0200

    Adding Mohamed's code.

--- src/Board.java
+++ src/Board.java
@@ -1,54 +1,12 @@
 abstract class Board {
+    protected int size;
+    protected char grid[][];
 
-    protected Integer n;
-    protected int moves = 0;
-    protected char[][] grid;
+    public abstract boolean update(int x, int y, char symbol);
 
-    public Board(Integer N) {
+    public abstract void displayBoard();
 
-        this.n = N;
-        grid = new char[n][n];
+    public abstract void clearBoard();
 
-        clearBoard();
-
-    }
-
-    public boolean update(Integer x, Integer y, char s) {
-
-        // check move not made before
-        if (grid[x][y] == MainUtils.DASH) {
-            grid[x][y] = s;
-            moves++;
-            return true;
-        }
-        return false;
-    }
-
-    public void displayBoard() {
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(grid[i][j]);
-                System.out.print(' ');
-            }
-            System.out.print('\n');
-        }
-
-    }
-
-    public void clearBoard() {
-
-        // resets the board
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                grid[i][j] = MainUtils.DASH;
-            }
-        }
-
-    }
-
-    public abstract boolean isWinner(char s);
-
-    public abstract boolean isDraw();
-
+    public abstract boolean isWinner();
 }
--- src/TicTacToeBoard.java
+++ src/TicTacToeBoard.java
@@ -1,71 +1,96 @@
-public class TicTacToeBoard extends Board {
-
-    public TicTacToeBoard(Integer N) {
-        super(N);
+public class TicTacToeBoard extends Board
+{
+    private int count = 0;
+    public TicTacToeBoard(int size)
+    {
+        this.size = size;
+        this.grid = new char[size][size];
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                grid[i][j] = ' ';   // set to empty state value
+            }
+        }
     }
 
-    public boolean isWinner(char s) {
-
-        boolean won = true;
-
-        // check rows - done
-        for (int i = 0; i < n; i++) {
-            won = true;
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] != s) {
-                    won = false;
-                    break;
+    public void clearBoard()
+    {
+        for(int i = 0; i < size; i++)
+        {
+            for(int j = 0; j < size; j++)
+            {
+                grid[i][j] = ' ';
+            }
+        }
+    }
+    public void displayBoard()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                if (j != size - 1)
+                {
+                    System.out.print(grid[i][j] + " | ");
+                }
+                else
+                {
+                    System.out.print(grid[i][j]);
                 }
             }
-            if (won) {
-                return true;
-            }
+            System.out.println();
         }
+    }
 
-        // check columns - done
-        for (int i = 0; i < n; i++) {
-            won = true;
-            for (int j = 0; j < n; j++) {
-                if (grid[j][i] != s) {
-                    won = false;
-                    break;
-                }
-            }
-            if (won) {
-                return true;
-            }
-
-        }
-
-        // check bottom right diagonal - done
-        won = true;
-        for (int i = 0; i < n; i++) {
-            if (grid[i][i] != s) {
-                won = false;
-                break;
-            }
-        }
-        if (won) {
+    public boolean update(int x, int y, char symbol)
+    {
+        if(grid[x][y] == ' ')       // empty state value
+        {
+            grid[x][y] = symbol;
+            count++;
             return true;
         }
-
-        // check bottom left diagonal - done
-        won = true;
-        for (int i = 0; i < n; i++) {
-            if (grid[n - i - 1][i] != s) {
-                won = false;
-                break;
-            }
+        else
+        {
+            return false;
         }
-        return won;
+    }
+    public boolean isWinner()
+    {
+        boolean win = false;
+        // horizontal check
+        for(int i = 0; i < size; i++)
+        {
+            win = (grid[i][0]) == (grid[i][1]) && grid[i][0] == grid[i][2] && grid[i][0] != ' ';
+            if(win){break;}
+        }
+        if(win){return win;}
+        // vertical check
+        for (int i = 0; i < size; i++)
+        {
+            win = (grid[0][i] == grid[1][i]) && ((grid[0][i] == grid[2][i]) && (grid[0][i] != ' '));
+            if(win){break;}
+        }
+        if(win){return win;}
+
+        // diagonal check
+
+        win = (grid[0][0]) == (grid[1][1]) && ((grid[0][0] == grid[2][2]) && (grid[0][0] != ' '));
+        if(win){return win;}
+
+        win = (grid[0][2]) == (grid[1][1]) && ((grid[0][2] == grid[2][0]) && (grid[0][2] != ' '));
+
+        return win;
     }
 
-    public boolean isDraw() {
-
-        if (moves == n * n) {
-            return !isWinner(MainUtils.X) && !isWinner(MainUtils.O);
+    public boolean isDraw()
+    {
+        if(count == 9)
+        {
+            System.out.println("Draw!");
+            return true;
         }
         return false;
     }
-
 }
